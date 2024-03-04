13bcb2493f142f055fe0d4b59c46ca4875dfbf65
김우현 <rladngus133@naver.com>
2024-03-04 15:03:16 +0900

    build: StoryBook + tailwind 연동

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,19 +1,27 @@
 import type { StorybookConfig } from "@storybook/nextjs";
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-onboarding",
-    "@storybook/addon-interactions",
-  ],
-  framework: {
-    name: "@storybook/nextjs",
-    options: {},
-  },
-  docs: {
-    autodocs: "tag",
-  },
+	stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+	addons: [
+		"@storybook/addon-links",
+		"@storybook/addon-essentials",
+		"@storybook/addon-onboarding",
+		"@storybook/addon-interactions",
+	],
+	framework: {
+		name: "@storybook/nextjs",
+		options: {},
+	},
+	docs: {
+		autodocs: "tag",
+	},
+	previewHead: (head) => `  
+  ${head}
+  <style>
+      html {
+        font-size: 62.5%;
+      }
+    </style>
+  `,
 };
 export default config;
