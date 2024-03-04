ba7488d126c7de2cbb7cbf0d4ebc4a0bed219b30
김우현 <rladngus133@naver.com>
2024-03-04 14:31:24 +0900

    build: 초기세팅
    - EsLint , Prettier 설정
    - Global-Font 셋팅
    - 기존파일 Prettier 규칙에따라 다시세이브

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,3 @@
 export default function Home() {
-  return (
-    <div className="w-16 md:w-20">
-      <p className="text-white">White</p>
-      <p className="text-black-100">Black 100</p>
-      <p className="text-black-200">Black 200</p>
-      <p className="text-black-300">Black 300</p>
-      <p className="text-gray-100">Gray 100</p>
-      <p className="text-gray-200">Gray 200</p>
-      <div className="bg-gradient-to-r from-main_gradation-start to-main_gradation-end">
-        <p className="text-white">Gradation</p>
-      </div>
-      <p className="text-main_blue">Main Blue</p>
-      <p className="text-main_indigo">Main Indigo</p>
-      <p className="text-yellow">Yellow</p>
-      <p className="text-green">Green</p>
-      <p className="text-pink">Pink</p>
-      <p className="text-red">Red</p>
-    </div>
-  );
+	return <div></div>;
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,38 +1,41 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        white: "#F1F1F5",
-        black: {
-          100: "#2E2E3A",
-          200: "#21212A",
-          300: "#17171C",
-        },
-        gray: {
-          100: "#9FA6B2",
-          200: "#6E6E82",
-        },
-        main_gradation: {
-          start: "#5097FA",
-          end: "#5363FF",
-        },
-        main_blue: "#5097FA",
-        main_indigo: "#5363FF",
-        yellow: "#FFC83C",
-        green: "#05D58B",
-        pink: "#FF2F9F",
-        red: "#FF0000",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			fontFamily: {
+				Pretendard: ["Pretendard"],
+			},
+			colors: {
+				white: "#F1F1F5",
+				black: {
+					100: "#2E2E3A",
+					200: "#21212A",
+					300: "#17171C",
+				},
+				gray: {
+					100: "#9FA6B2",
+					200: "#6E6E82",
+				},
+				main_gradation: {
+					start: "#5097FA",
+					end: "#5363FF",
+				},
+				main_blue: "#5097FA",
+				main_indigo: "#5363FF",
+				yellow: "#FFC83C",
+				green: "#05D58B",
+				pink: "#FF2F9F",
+				red: "#FF0000",
+			},
+		},
+	},
+	plugins: [],
 };
 export default config;
 
