959634396e5bbe4c92c00e3e613397afc601be13
woohyun kim <rladngus133@naver.com>
2024-03-13 17:08:39 +0900

    feat: TanStackQuery Provider 및 공통스타일 헤더추가 (#80)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,30 @@
 import "@/styles/globals.css";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import type { AppProps } from "next/app";
+import React, { ReactNode } from "react";
+
+import Header from "@/components/common/menu/Header";
+
+const queryClient = new QueryClient();
+
+function Providers({ children }: { children: ReactNode }) {
+	return (
+		<QueryClientProvider client={queryClient}>
+			{children}
+			<ReactQueryDevtools initialIsOpen={false} />
+		</QueryClientProvider>
+	);
+}
 
 export default function App({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+	return (
+		<>
+			<Header />
+			<Providers>
+				<Component {...pageProps} />
+			</Providers>
+		</>
+	);
 }
